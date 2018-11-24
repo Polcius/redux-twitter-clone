d4a1662a0ffa4c0ba225c0286b6a0e34ae954671
Pol Milian <polcius@gmail.com>
2018-11-24 17:51:10 +0930

    dahsboard component to display tweet ids

---
+++ src/components/Dashboard.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="center">Your Timeline</h3>
+        <ul className="dashboard-list">
+          {this.props.tweetIds.map(id => (
+            <li key={id}>
+              <div>TWEET ID: {id}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+// mapStateToProps is the way to retrieve data from the store. The name says it all. We need to connect it in the end. 
+
+function mapStateToProps({ tweets }) {
+  return {
+    tweetIds: Object.keys(tweets).sort(
+      (a, b) => tweets[b].timestamp - tweets[a].timestamp
+    )
+  };
+}
+
+export default connect(mapStateToProps)(Dashboard);
